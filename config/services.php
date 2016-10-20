77c8dd0e1dec6c56ba69a6ca3ddd5f22cf29be30
Vasilich <vasilich@grafin.kiev.ua>
2016-10-20 20:43:54 +0300

    Further improvements

--- config/services.php
+++
@@ -1,8 +0,0 @@
-<?php
-return [
-  'google' => [
-    'client_id' => '693705279651-hmg32ua0noqagbpet4hjg05agbo2vqik.apps.googleusercontent.com',
-    'client_secret' => '9t6isX5HNuR4FlfKtey5QSB5',
-    'redirect' => 'http://camcase.com/callback',
-  ],
-];
